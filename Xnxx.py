fbc80786fc91ff4fb8a33edae57d7975a05bc17f
X4CY <66755146+X4CY@users.noreply.github.com>
2020-06-11 20:31:32 +0700

    Create Xnxx.py

---
+++ Xnxx.py
@@ -0,0 +1,65 @@
+#!/bin/bash#
+Author : X4-CY
+IDclearecho "|------------------------------------------|"sleep 1echo " TOOLS HACK FB TARGET BY X4-CY 
+"echo "|------------------------------------------|"
+r="\033[1;31m"
+g="\033[1;32m"
+y="\033[1;33m"
+pur="\033[1;34m"
+p="\033[1;35m"
+c="\033[1;36m"
+w="\033[1;37m"
+hari=`date +%A`
+tanggal=`date +%d`
+bulan=`date +%B`
+tahun=`date +%Y`
+waktu=`date +%T`
+zona=`date +%Z`
+sleep 1echo ""echo ""echo "
+${g}+------------------------------------------+"echo "Selamat Datang Kawan Semoga gak chekpoint"
+echo "+------------------------------------------+"sleep 1echo "
+"echo "${r}+------------------------------------------+
+"echo "Jangan Salah Gunakan Tools Ini
+"echo "+------------------------------------------+"sleep 1echo "
+"echo "${y}+------------------------------------------+
+"echo "Sebelumnya Kita Boleh kenalan Dulu 
+"sleep 2echo -n "Siapakah Nama Kamu ? ";read nmsleep 
+1echo "Hallo $nm Senang bisa berkenalan denganmu :) 
+"sleep 2echo "Eh iya Lupa Perkenalkan Nama Saya X4-CY 
+"sleep 2echo "Sudah Cukup Perkenalannya ya 
+"sleep 
+1echo "+------------------------------------------+
+"sleep 1echo 
+"${r}Oke Lanjut .... 
+"sleep 1read -p "Apakah Anda Ingin Menggunakan Tools Ini[y/n]: 
+" ynif [ $yn = y ]thenclearsleep 1echo 
+"${c}|---------------------------------------------|"echo 
+"${c}| Tools Hack Fb Target |"echo 
+"${c}|---------------------------------------------|"echo 
+"${c}|(/) Author : X4-CY |
+"echo 
+"${c}|(/)  : ://github |"echo 
+"${c}|(/) fanspage : https:/fb.
+"echo 
+"${c}|(/) Copyright : ©2020.inc |"echo "${c}| |"echo 
+"${c}|---------------------------------------------|
+"sleep 2echo ""echo "(/)Sedang memproses tools...
+"sleep 3echo ""echo "(/)Mengupload Tools...
+"sleep 3echo ""echo "(/)Mengekstrax Tools...
+"sleep 5echo ""echo "(/)Membuka Tools...
+"sleep 3echo ""elsesleep 1echo ""echo 
+"${g}Terima kasih Telah Mengunjungi Tools ini
+"sleep 1echo "
+"echo 
+"${c}(/) Sedang Proses Keluar ... 
+"sleep 3exitficlearsleep 2echo 
+"${c}|---------------------------------------------|
+"echo "${c}| Tools Hack Fb Target |"echo
+"${c}|---------------------------------------------|"echo 
+"${c}|(/) Author : X4-CY |"echo "${c}|(/) Copyright : ©2020.inc |"echo "${c}| |"echo "${c}|---------------------------------------------|"echo ""read -p "Apakah Anda Ingin Melanjutkan [y/n] " nnif [ $nn = y ]thensleep 2echo "Login Dulu Menggunakan Akun Fb Anda "echo -n "Masukan Username/id fb : ";read uuecho -n "Masuka Password fb kamu: ";read iiecho "(/)Memeriksa Username Dan Password..."sleep 4echo "(/)Proses Masuk Akun Anda..."termux-brightness 0termux-brightness 200termux-brightness 0termux-brightness 200sleep 4echo "(+)Login [suscces] "sleep 1termux-brightness 0termux-brightness 200termux-brightness 0termux-brightness 200echo "Langkah Terakhir.. "termux-brightness 0termux-brightness 200termux-brightness 0termux-brightness 200sleep 1echo "Cari Id Korban Lalu masukan di Sini... "elseexitfiecho -n "Masukan Id Korban : ";read idecho "(/) Membobol target $id untuk mendapatkan password ... "sleep 6echo "Password Target di Temukan..
+"sleep 2echo "+---------------------+
+id = $id
+pw = Alanwalker3782+---------------------+" 
+*sleep 1echo 
+"Done ....
+"sleep
